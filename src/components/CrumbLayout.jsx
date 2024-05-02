9c032bd37ffec50dae9e6b7679b36ca52f209b54
Boluwatife Ade-Ojo <adeojopeter@gmail.com>
2024-05-02 11:16:32 +0100

    updates

--- src/components/CrumbLayout.jsx
+++ src/components/CrumbLayout.jsx
@@ -2,35 +2,42 @@ import { Link, Outlet, useLocation } from 'react-router-dom';
 import { BiRightArrow, BiPointer } from 'react-icons/bi';
 import { FaAngleRight } from 'react-icons/fa';
 
+const BreadCrumb = ({ location }) => {
+	return (
+		<div className="flex text-[#ffffffad] items-center gap-x-2 font-light pt-12 pb-8 px-4 md:px-0">
+			<Link to={'/'} className="hover:text-white transition-colors">
+				Home
+			</Link>
+			{location.pathname
+				.split('/')
+				.slice(1)
+				.map((t, i) => {
+					return (
+						<span key={t} className="flex items-center gap-x-2">
+							<FaAngleRight />
+							<Link
+								to={location.pathname
+									.split('/')
+									.slice(0, i + 2)
+									.join('/')}
+								className="capitalize hover:text-white transition-colors"
+							>
+								{t}
+							</Link>
+						</span>
+					);
+				})}
+		</div>
+	);
+};
+
 const CrumbLayout = () => {
 	const location = useLocation();
 	return (
 		<>
-			<div className="flex text-[#ffffffad] items-center gap-x-2 font-light pt-12 pb-8 px-4 md:px-0">
-				<Link to={'/'} className="hover:text-white transition-colors">
-					Home
-				</Link>
-				{location.pathname
-					.split('/')
-					.slice(1)
-					.map((t, i) => {
-						return (
-							<span key={t} className="flex items-center gap-x-2">
-								<FaAngleRight />
-								<Link
-									to={location.pathname
-										.split('/')
-										.slice(0, i + 2)
-										.join('/')}
-									className="capitalize hover:text-white transition-colors"
-								>
-									{t}
-								</Link>
-							</span>
-						);
-					})}
-			</div>
+			<BreadCrumb location={location} />
 			<Outlet />
+			<BreadCrumb location={location} />
 		</>
 	);
 };
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,7 +4,7 @@ const About = () => {
 			<div className="grid md:grid-cols-2 gap-x-10 min-h-min">
 				<div className="">
 					<p className="uppercase text-3xl pb-4 text-white">who is udoka?</p>
-					<p className="text-justify">
+					<p className="text-center md:text-justify">
 						Udoka (aka yuddy) is an ever evolving UI/UX designer with a strong
 						background in Metallurgical & Materials Engineering
 						<br />
@@ -19,6 +19,7 @@ const About = () => {
 						background in engineering, I bring a holistic approach to every
 						project, ensuring that each design decision serves a purpose.
 					</p>
+					bun dev
 				</div>
 				<img src="" className="bg-white block" alt="" />
 			</div>
@@ -31,7 +32,7 @@ const About = () => {
 			<div className="py-4"></div>
 			<div className="min-h-[400px] bg-white"></div>
 			<div className="py-4"></div>
-			<div  className="text-center text-[#ffffffad]">
+			<div className="text-center text-[#ffffffad]">
 				What sets me apart is my commitment to creating not just visually
 				appealing designs, but experiences that resonate with users on a
 				personal level. This portfolio is not just a showcase of his work, it's
@@ -40,7 +41,8 @@ const About = () => {
 				interactions spark emotions, and design transcends the screen. Udoka’s
 				toolbox includes a proficiency in Figma, a little bit of Adobe XD and
 				Photoshop as well as Auto CAD (inventor), allowing me to seamlessly
-				blend creativity with technical prowess and also render these <span className="text-white">SERVICES</span>
+				blend creativity with technical prowess and also render these{' '}
+				<span className="text-white">SERVICES</span>
 			</div>
 		</div>
 	);
